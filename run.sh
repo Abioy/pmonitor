20798bbf810e9fa705cee84dbefd16323b96a596
Yongyue Sun <abioy.sun@gmail.com>
2017-08-05 13:36:42 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,10 @@
-( cd /home/abioy/local/heater; echo `date` >> log/heater.log ; flock -n log/run.lock bash smzdm.sh >> log/heater.log 2>&1 & )
-
 cd /home/abioy/local/heater
+
+bash clean.sh
+
+echo `date` >> log/heater.log
 bash pull.sh smzdm.sh >> log/heater.log
+
+sleep 1
+
+( cd /home/abioy/local/heater; flock -n log/run.lock bash smzdm.sh >> log/heater.log 2>&1 & )
